e342388dc185d6080e2d51faf2f755a045bf8b13
kitarp29 <kitarpsinghrajpoot@gmail.com>
2022-11-11 12:53:08 +0530

    Making it modular
    Signed-off-by: kitarp29 <kitarpsinghrajpoot@gmail.com>

--- main.go
+++ main.go
@@ -24,34 +24,46 @@ func main() {
 	}))
 	// Root route => handler
 	e.GET("/", func(c echo.Context) error {
-		var response string
-		uuid := c.QueryParam("u")
-		for key, value := range ListMap {
-			if key == uuid {
-				return c.Redirect(http.StatusMovedPermanently, value)
-			} //response = response + key + " : " + value
-		}
-		response = "No URL found"
-		return c.String(http.StatusOK, response)
+		return Redirect(c)
 	})
 
 	e.POST("addlink", func(c echo.Context) error {
-		var response string
-		link := c.QueryParam("link")
-		newkey := shortuuid.New()
-		ListMap[newkey] = link
-		response += "http://localhost:8000/?u=" + newkey
-		return c.String(http.StatusOK, response)
+		return AddLink(c)
 	})
 
 	e.GET("list", func(c echo.Context) error {
-		var response string
-		for key, value := range ListMap {
-			response = response + key + " : " + value
-		}
-		return c.String(http.StatusOK, response)
+		return List(c)
 	})
 
 	// Run Server
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func Redirect(c echo.Context) error {
+	var response string
+	uuid := c.QueryParam("u")
+	for key, value := range ListMap {
+		if key == uuid {
+			return c.Redirect(http.StatusMovedPermanently, value)
+		} //response = response + key + " : " + value
+	}
+	response = "No URL found"
+	return c.String(http.StatusOK, response)
+}
+
+func AddLink(c echo.Context) error {
+	var response string
+	link := c.QueryParam("link")
+	newkey := shortuuid.New()
+	ListMap[newkey] = link
+	response += "http://localhost:8000/?u=" + newkey
+	return c.String(http.StatusOK, response)
+}
+
+func List(c echo.Context) error {
+	var response string
+	for key, value := range ListMap {
+		response = response + key + " : " + value
+	}
+	return c.String(http.StatusOK, response)
 }
